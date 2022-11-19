6a2da7dbf38142e1425ef8e1a4dab03736e3bb9a
akmalova <karina130901@gmail.com>
2022-11-19 04:09:11 +0300

    Revert "navigation menu created"
    This reverts commit bb86a15d1a411eec3030f2b9381cb68d62eacabe.

--- lib/utils/app_colors.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-abstract class AppColors {
-  static const grey = Color.fromRGBO(189, 189, 189, 1);
-  static const darkBlue1 = Color.fromRGBO(44, 60, 86, 1);
-  static const darkBlue2 = Color.fromRGBO(40, 54, 78, 1);
-  static const white = Colors.white;
-  static const red = Color.fromRGBO(227, 30, 37, 1);
-}
